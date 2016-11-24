5b45ee18a0f432b33c469e7b2fa6270ea04daccb
syh03 <1336840590@qq.com>
2016-11-24 17:46:15 +0800

    selenium_fist

--- Selenium/learning_foundation/first.py
+++ Selenium/learning_foundation/first.py
@@ -7,8 +7,13 @@ Created on 2016/9/20
 
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get("http://yanzijia.cn")
+browser = webdriver.Ie()
 
 
+# browser = webdriver.Chrome()
+browser.get("http://www.baidu.com")
+browser.find_element_by_id('kw').send_keys("selenium")
+browser.find_element_by_id("su").click()
+
+# browser.refresh()
 browser.quit()
