5ad65f98ac6401ac4c00da211ca5a43f7d93bd99
syh03 <1336840590@qq.com>
2016-12-21 17:35:31 +0800

    tt

--- Selenium/learning_foundation/first.py
+++ Selenium/learning_foundation/first.py
@@ -9,11 +9,18 @@ from selenium import webdriver
 
 browser = webdriver.Ie()
 
+# "浏览器最大化"
+# browser.maximize_window()
 
-# browser = webdriver.Chrome()
-browser.get("http://www.baidu.com")
-browser.find_element_by_id('kw').send_keys("selenium")
-browser.find_element_by_id("su").click()
+# 设置浏览器宽高
+browser.set_window_size(600, 800)
+
+url = "http://devsheji.yanzijia.cn/m/login"
+browser.get(url)
+
+browser.find_element_by_id('uname').send_keys("admin")
+browser.find_element_by_id("pwd").send_keys("123")
+browser.find_element_by_id("J-login-btn").click()
 
 # browser.refresh()
-browser.quit()
+# browser.quit()
--- Selenium/tt.py
+++ Selenium/tt.py
@@ -4,12 +4,34 @@ Created on 2016/11/2
 
 @author: sunyihuan
 '''
+from appium import webdriver
+#
+# def f(x,y):
+#     z=x+y
+#     return z
+# print f(1,3)
+
+# -*- coding:utf-8 -*-
+# 用python实现排列组合C(n,m) = n!/m!*(n-m)!
+def get_value(n):
+    if n==1:
+        return n
+    else:
+        return n * get_value(n-1)
+
+def gen_last_value(n,m):
+     first = get_value(n)
+     print "n:%s     value:%s"%(n, first)
+     second = get_value(m)
+     print "n:%s     value:%s"%(m, second)
+     third = get_value((n-m))
+     print "n:%s     value:%s"%((n-m), third)
+     return first/(second * third)
 
 
-def f(x,y):
-    z=x+y
-    return z
-print f(1,3)
+if __name__ == "__main__":
+    # C(12,5)
+    rest = gen_last_value(15,5)
+    print "value:", rest
 
-
-
+print 15*14*13*12*11/(5*4*3*2)
