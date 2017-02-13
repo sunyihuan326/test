aacf52211664a75d8afdb1ff49cf429002cf34e1
syh03 <1336840590@qq.com>
2017-02-13 10:32:35 +0800

    tt

--- Selenium/learning_foundation/tt.py
+++ Selenium/learning_foundation/tt.py
@@ -5,11 +5,16 @@ Created on 2016/10/17
 @author: sunyihuan
 '''
 import math
-a=math.sqrt(5*5*5)
-b=math.sqrt(math.sqrt(a))
-print  b,200/b
-
-print pow(1.83,8)
-print 109.37*pow(1.828,1)
-print 109.37*pow(1.828,9)
-print 100*pow(2,9)
+print 7471*7.9+10*281*9.7+30000
+print float(2810*5)/6371
+print 2810*0.85/4.9
+print 30000*(20+5.0/12)/20000
+print float(281*14)/(8*250)
+# a=math.sqrt(5*5*5)
+# b=math.sqrt(math.sqrt(a))
+# print  b,200/b
+#
+# print pow(1.83,8)
+# print 109.37*pow(1.828,1)
+# print 109.37*pow(1.828,9)
+# print 100*pow(2,9)
---
+++ t.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+'''
+Created on 2017/1/4
+
+@author: sunyihuan
+'''
+
+a = 3.11
+b = str(a)
+print b
