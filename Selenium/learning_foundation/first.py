3e16cd416c7734d9d5d164b310fa1377249d84d1
syh03 <1336840590@qq.com>
2017-04-17 20:46:13 +0800

    first selenium

--- Selenium/learning_foundation/first.py
+++ Selenium/learning_foundation/first.py
@@ -13,15 +13,15 @@ browser = webdriver.Ie()
 # browser.maximize_window()
 
 # 设置浏览器宽高
-browser.set_window_size(600, 800)
+# browser.set_window_size(600, 800)
 
 url = "http://devsheji.yanzijia.cn/m/login"
 browser.get(url)
 
-browser.find_element_by_id('uname').send_keys("admin")
-browser.find_element_by_id("pwd").send_keys("123")
+browser.find_element_by_id('account').send_keys("lby")
+browser.find_element_by_id("password").send_keys("123456")
 browser.find_element_by_id("J-login-btn").click()
 
 
 # browser.refresh()
-# browser.quit()
+browser.quit()
--- python-appium/Just_Play/first_test.py
+++ python-appium/Just_Play/first_test.py
@@ -9,23 +9,23 @@ desired_caps['appPackage'] = 'com.android.calculator2'
 desired_caps['appActivity'] = '.Calculator'
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-#
-# driver.find_element_by_name("1").click()
-#
-# driver.find_element_by_name("5").click()
-#
-# driver.find_element_by_name("9").click()
-#
-# driver.find_element_by_name("delete").click()
-#
-# driver.find_element_by_name("9").click()
-#
-# driver.find_element_by_name("5").click()
-#
-# driver.find_element_by_name("+").click()
-#
-# driver.find_element_by_name("6").click()
-#
-# driver.find_element_by_name("=").click()
+
+driver.find_element_by_name("1").click()
+
+driver.find_element_by_name("5").click()
+
+driver.find_element_by_name("9").click()
+
+driver.find_element_by_name("delete").click()
+
+driver.find_element_by_name("9").click()
+
+driver.find_element_by_name("5").click()
+
+driver.find_element_by_name("+").click()
+
+driver.find_element_by_name("6").click()
+
+driver.find_element_by_name("=").click()
 
 driver.quit()
